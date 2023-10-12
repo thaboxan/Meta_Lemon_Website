54494562d38293120ae8c3bf3cfa5ae9b739d4e9
thaboxan <thvbojafta@gmail.com>
2023-10-12 20:36:23 +0200

    structure setup

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+    return (
+      <header>
+        <img src={logo} alt="Little Lemon Logo" />
+      </header>
+    );
+  }
+
+  export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Main(){
+    return(
+    <main>
+    </main>
+
+    )
+}
+
+
+
+export default Main
